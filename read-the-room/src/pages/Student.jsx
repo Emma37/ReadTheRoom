d7cd6eede313b877c19750fae83bee9b2c6871a2
Emma37 <17068150+Emma37@users.noreply.github.com>
2021-01-23 23:35:46 +0000

    theming

--- read-the-room/src/pages/Student.jsx
+++ read-the-room/src/pages/Student.jsx
@@ -36,34 +36,46 @@ const Student = () => {
 
     return (
         <>
-        <h1>Class: Algebra 28/01/2021</h1>
+        <h1 className="text-center mb-4">Class: Algebra 28/01/2021</h1>
 
-        <div id="container">
-            <video autoPlay playsInline id="videoElement" className="bg-dark" ref={videoRef}>
-            </video>
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-6">
+                    <div>
+                        <div className="bg-secondary p-2">
+                            Only you can see this video stream
+                        </div>
+                        <div id="container">
+                            <video autoPlay playsInline id="videoElement" className="webcam-feed bg-dark" ref={videoRef}>
+                            </video>
+                        </div>
+                    </div>
+                    <select>
+                        {devices.map((value, index) => {
+                            return <option>{value}</option>
+                        })}
+                        {/* {items} */}
+                    </select>
+                </div>
+                <div className="col-lg-6">
+                <div>
+                    <div>This is the data being sent to your teacher and will be aggregated with everyone from your class</div>
+                        Emotion
+                    </div>
+                    <div>
+                        <div>Need to tell your teacher something?</div>
+                        <SpeechButton text="Please slow down" />
+                        <SpeechButton text="You're on mute" />
+                        <SpeechButton text="There's a bad internet connection" />
+                        <SpeechButton text="I'm really confused" />
+                    </div>
+                </div>
+            </div>
         </div>
-        <select>
-            {devices.map((value, index) => {
-                return <option>{value}</option>
-            })}
-            {/* {items} */}
-        </select>
 
-        <div>
-            No one can see this video stream but you
-        </div>
 
-        <div>
-            <div>This is the data being sent to your teacher and will be aggregated with everyone from your class</div>
-            Emotion
-        </div>
-        <div>
-            <div>Need to tell your teacher something?</div>
-            <SpeechButton text="Please slow down" />
-            <SpeechButton text="You're on mute" />
-            <SpeechButton text="There's a bad internet connection" />
-            <SpeechButton text="I'm really confused" />
-        </div>
+
+
         </>
     )
 }
