5aba5c2437820f8dae9b50d35e4411c951fa56f3
Taketomo <ti250@cam.ac.uk>
2021-01-24 03:30:23 +0900

    let's just upload the build folder lol

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>New South 4eva!</h1>"
+    return send_from_directory("read-the-room/build", "index.html")
+    # return "<h1>New South 4eva!</h1>"
 
 
 @app.route("/test")
@@ -32,6 +33,31 @@ def update_data():
     print(given_data)
     return
 
+
+@app.route("/<path:path>")
+@app.route("/<string:path>")
+def catch_all(path):
+    print(path)
+    return send_from_directory("read-the-room/build", path)
+
+
+@app.route("/static/js/<path:path>")
+@app.route("/static/js/<string:path>")
+def catch_static_js(path):
+    print(path)
+    return send_from_directory("read-the-room/build/static/js", path)
+
+@app.route("/static/css/<path:path>")
+@app.route("/static/css/<string:path>")
+def catch_static_css(path):
+    print(path)
+    return send_from_directory("read-the-room/build/static/css", path)
+
+@app.route("/static/media/<path:path>")
+@app.route("/static/media/<string:path>")
+def catch_static_media(path):
+    print(path)
+    return send_from_directory("read-the-room/build/static/media", path)
 
 if __name__ == "__main__":
     app.run(debug=True)
