c6511a9f8e7f2065b2e40a643d638f4e398074ff
Taketomo <ti250@cam.ac.uk>
2021-01-24 02:06:28 +0900

    Add dummy endpoints

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, send_from_directory
+import json
 
 app = Flask(__name__)
 
@@ -18,9 +19,18 @@ def test_json():
     return {"test": 1}
 
 
-@app.route("/<path:path>")
-def home(path):
-    return send_from_directory("client/public", path)
+@app.route("/current_data")
+def send_current_data():
+    return {"users": {"total": 10, "happy": 1, "confused": 9, "active": 10},
+            "commands": {"slow": 1, "muted": 0}
+            }
+
+
+@app.route("/send_data", methods=["POST"])
+def update_data():
+    given_data = json.loads(request.data)
+    print(given_data)
+    return
 
 
 if __name__ == "__main__":
