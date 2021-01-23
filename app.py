2a17cbd40b75026575b8673b41ef72a63d380a5c
Taketomo <ti250@cam.ac.uk>
2021-01-24 04:01:15 +0900

    Filtering should in theory work?
    -Need to check with some trials on Postman

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, send_from_directory
 from collections import Counter
 import json
+import datetime
 
 app = Flask(__name__)
 
 students = {}
+timeout_time = 10
 
 @app.route("/")
 def index():
@@ -22,8 +24,17 @@ def test_json():
     return {"test": 1}
 
 
+def clean_students_dictionary():
+    global students
+    current_date = datetime.datetime.now()
+    invalid_keys = [s for s in students if (current_date - students[s]["date"]).total_seconds() > timeout_time]
+    for key in invalid_keys:
+        students.pop(key, None)
+
+
 @app.route("/current_data")
 def send_current_data():
+    clean_students_dictionary()
     emotion_counts = Counter(students[s]["main_emotion"] for s in students)
     emotion_counts["total_students"] = len(students.keys())
     # active_students calculated on some timeout function + no students detected?
@@ -36,6 +47,7 @@ def send_current_data():
 @app.route("/send_data", methods=["POST"])
 def update_data():
     given_data = json.loads(request.data)
+    given_data['date'] = datetime.datetime.now()
     students[given_data['id']] = given_data
     return
 
@@ -53,17 +65,20 @@ def catch_static_js(path):
     print(path)
     return send_from_directory("read-the-room/build/static/js", path)
 
+
 @app.route("/static/css/<path:path>")
 @app.route("/static/css/<string:path>")
 def catch_static_css(path):
     print(path)
     return send_from_directory("read-the-room/build/static/css", path)
 
+
 @app.route("/static/media/<path:path>")
 @app.route("/static/media/<string:path>")
 def catch_static_media(path):
     print(path)
     return send_from_directory("read-the-room/build/static/media", path)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
