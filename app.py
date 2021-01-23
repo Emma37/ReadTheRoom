c87f2277e74c0cd6f88591417ec44e88de6bed11
Matthew Bradley <Matthew Bradley matthew.bradley@cern.ch>
2021-01-23 18:44:16 +0000

    Update and send students emotions

--- app.py
+++ app.py
@@ -1,37 +1,34 @@
 from flask import Flask, send_from_directory
+from collections import Counter
 import json
 
 app = Flask(__name__)
 
+students = {}
 
 @app.route("/")
 def index():
     return "<h1>New South 4eva!</h1>"
 
-
-@app.route("/test")
-def test():
-    return "<h1>Matt's learning Flask!</h1>\n Hello world!!"
-
-
 @app.route("/json")
 def test_json():
     return {"test": 1}
 
-
 @app.route("/current_data")
 def send_current_data():
-    return {"users": {"total": 10, "happy": 1, "confused": 9, "active": 10},
-            "commands": {"slow": 1, "muted": 0}
+    emotion_counts = Counter(students[s]["main_emotion"] for s in students)
+    emotion_counts["total_students"] = len(students.keys())
+    # active_students calculated on some timeout function?
+    command_counts = Counter(students[s]["active_command"] for s in students)
+    return {"users": emotion_counts,
+            "commands": command_counts
             }
-
 
 @app.route("/send_data", methods=["POST"])
 def update_data():
     given_data = json.loads(request.data)
-    print(given_data)
+    students{given_data['id']} = given_data
     return
-
 
 if __name__ == "__main__":
     app.run(debug=True)
