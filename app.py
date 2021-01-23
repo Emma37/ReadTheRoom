c0a71abbbb7a66664f42d78a605862e0d6ff82bd
Taketomo <ti250@cam.ac.uk>
2021-01-24 00:49:25 +0900

    Add some other stuff

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from flask import Flask
+from flask import Flask, send_from_directory
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "<h1>Hello Azure!</h1>"
+    return "<h1>New South 4eva!</h1>"
+
+
+@app.route("/test")
+def test():
+    return "<h1>Matt's learning Flask!</h1>\n Hello world!!"
+
+
+@app.route("/json")
+def test_json():
+    return {"test": 1}
+
+
+@app.route("/<path:path>")
+def home(path):
+    return send_from_directory("client/public", path)
